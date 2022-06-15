9e06e001a541e56e52cebbbcc49cbaf5c534a9bc
GaabrielFranca <gabrielfran1864@gmail.com>
2022-06-15 02:53:54 -0300

    LastVersion

--- js/scripts.js
+++ js/scripts.js
@@ -31,19 +31,76 @@ _soundUrls.forEach(sndPath => {
 	_data.sounds.push(audio);
 });
 
-_gui.switch.addEventListener("click", () => {
 
+
+_gui.switch.addEventListener("click", () => {
+	_data.gameOn = _gui.switch.classList.toggle("gui__btn-switch--on")
+	_gui.counter.classList.toggle("gui__counter--on")
+	_gui.counter.innerHTML = "--"
+	_gui.led.classList.remove("gui__led--active")
+
+	_data.strict = false
+	_data.playerCanPlay = false
+	_data.score = 0
+	_data.gameSequence = []
+	_data.playerSequence = []
+
+	disablePads()
 });
 
+console.log(_data.gameOn)
 _gui.strict.addEventListener("click", () => {
+	if (!_data.gameOn)
+		return
+
+	let strictOn = _gui.led.classList.toggle("gui__led--active")
+
+	if (strictOn) {
+		_data.strict = true
+	}
+	if (!strictOn) {
+		_data.strict = false
+	}
 
 });
 
 _gui.start.addEventListener("click", () => {
+	startGame()
 
 });
 
 const padListener = (e) => {
+	if (!_data.playerCanPlay)
+		return
+
+	let soundsId
+	_gui.pads.forEach((pad, key) => {
+		if (pad === e.target)
+			soundsId = key
+
+	})
+
+	e.target.classList.add("game__pad--active")
+	_data.sounds[soundsId].play();
+	_data.playerSequence.push(soundsId)
+
+	setTimeout(() => {
+		e.target.classList.remove("game__pad--active")
+
+		const currentMove = _data.playerSequence.length - 1
+
+		if (_data.playerSequence[currentMove] !== _data.gameSequence[currentMove]) {
+			_data.playerCanPlay = false
+			disablePads()
+			resetOrPlayAgain()
+		}
+		else if (currentMove === _data.gameSequence.length - 1) {
+			newColor()
+			playSequence()
+		}
+
+	}, 350)
+
 
 }
 
@@ -52,37 +109,151 @@ _gui.pads.forEach(pad => {
 });
 
 const startGame = () => {
+	blink("--", () => {
+
+		newColor()
+
+		playSequence()
+	})
+
 
 }
 
 const setScore = () => {
+	const score = _data.score.toString()
+	// const display = "00".substring(0, 2 - score.length) + score
 
+	_gui.counter.innerHTML = "0" + score
+
+	if (score >= 10) {
+		_gui.counter.innerHTML = score
+	}
 }
 
 const newColor = () => {
+	if (_data.score === 20) {
+		blink("**", startGame)
+		return
+	}
+	_data.gameSequence.push(Math.round(Math.random() * 3))
+	_data.score++
+
+	setScore()
 
 }
 
 const playSequence = () => {
+	let counter = 0,
+		padOn = true
+
+	_data.playerSequence = []
+	_data.playerCanPlay = false
+
+	const interval = setInterval(() => {
+
+		if (padOn) {
+			if (!_data.gameOn) {
+				clearInterval(interval)
+				disablePads()
+				return
+			}
+
+			if (counter === _data.gameSequence.length) {
+				clearInterval(interval)
+				disablePads()
+				waitForPlayerClick()
+				_data.playerCanPlay = true
+				return
+			}
+
+
+			const sndID = _data.gameSequence[counter];
+			const pad = _gui.pads[sndID]
+			console.log(pad)
+
+			_data.sounds[sndID].play()
+			pad.classList.add("game__pad--active")
+			counter++;
+		}
+		else {
+			disablePads()
+		}
+
+		padOn = !padOn;
+	}, 750)
+
 
 }
 
+
 const blink = (text, callback) => {
+
+	let counter = 0,
+		on = true;
+
+	_gui.counter.innerHTML = text
+
+	const interval = setInterval(() => {
+		if (!_data.gameOn) {
+			clearInterval(interval)
+			_gui.counter.classList.remove("gui__conunter--on")
+			return
+		}
+
+		if (on) {
+			_gui.counter.classList.remove("gui__counter--on")
+
+		}
+		if (!on) {
+			_gui.counter.classList.add("gui__counter--on")
+			if (counter++ === 3) {
+				clearInterval(interval)
+				callback()
+			}
+
+		}
+		on = !on;
+
+	}, 250)
 
 }
 
 const waitForPlayerClick = () => {
+	clearTimeout(_data.timeout)
 
+	_data.timeout = setTimeout(() => {
+		if (!_data.playerCanPlay)
+			return
+
+		disablePads()
+		resetOrPlayAgain()
+	}, 4000)
 }
 
 const resetOrPlayAgain = () => {
+	_data.playerCanPlay = false
 
+	if (_data.strict) {
+		blink("!!", () => {
+			_data.score = 0
+			_data.gameSequence = []
+			startGame()
+		})
+	}
+	else {
+		blink("!!", () => {
+			setScore()
+			playSequence()
+		})
+	}
 }
 
-const changePadCursor = (cursorType) => {
+// const changePadCursor = (cursorType) => {
 
-}
+// }
 
 const disablePads = () => {
-
+	_gui.pads.forEach(pad => {
+		pad.classList.remove("game__pad--active")
+	})
 }
